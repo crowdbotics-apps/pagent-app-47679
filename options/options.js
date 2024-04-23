fb8ef804e08a12d40656160a5d1f80ce0380f935
Crowdbotics <team@crowdbotics.com>
2024-04-23 17:47:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pagent-app-47679",
+  url: "https://pagent-app-47679.botics.co",
+  api: "https://pagent-app-47679.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
